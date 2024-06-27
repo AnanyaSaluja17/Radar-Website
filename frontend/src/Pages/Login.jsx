37c11087c760a7a324bc5adb0a2db037f45c2d36
Krmjn09 <kmahajan1_be22@thapar.edu>
2024-06-27 22:06:07 +0530

    commited

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -7,6 +7,7 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import background from "../assets/backgroundimage.jpeg";
 
 const Login = () => {
   const {
@@ -21,88 +22,106 @@ const Login = () => {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      sx={{
-        width: "100%",
-        maxWidth: "500px",
-        margin: "auto",
-        padding: "20px",
-        borderRadius: "8px",
-        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
-        backgroundColor: "white",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute",
-        top: "50%",
-        right: "5%",
-        transform: "translateY(-50%)",
-        "@media (max-width: 600px)": {
-          right: "2%",
-        },
-      }}
-    >
-      <Typography variant="h5" component="div" sx={{ mb: 2 }}>
-        Login Form
-      </Typography>
-      <TextField
-        fullWidth
-        label="Username"
-        {...register("username", {
-          required: "Username is required",
-          minLength: {
-            value: 3,
-            message: "Username must be at least 3 characters",
+    <>
+    
+      <div>
+        <img
+          src={background}
+          alt="background"
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            height: "100vh",
+            width: "100vw",
+            zIndex: -1,
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+        sx={{
+          width: "100%",
+          maxWidth: "500px",
+          margin: "auto",
+          padding: "20px",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          top: "50%",
+          right: "5%",
+          transform: "translateY(-50%)",
+          "@media (max-width: 600px)": {
+            right: "2%",
           },
-        })}
-        error={Boolean(errors.username)}
-        helperText={errors.username?.message}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        type="password"
-        label="Password"
-        {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 6,
-            message: "Password must be at least 6 characters",
-          },
-        })}
-        error={Boolean(errors.password)}
-        helperText={errors.password?.message}
-        margin="normal"
-        sx={{ mt: 2 }}
-      />
-      <FormControlLabel
-        control={<Checkbox {...register("rememberMe")} color="primary" />}
-        label="Remember Me"
-        sx={{ mt: 1, textAlign: "left" }}
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        fullWidth
-        sx={{ mt: 2 }}
+        }}
       >
-        Login
-      </Button>
-      <Box sx={{ mt: 2, textAlign: "center" }}>
-        <Link href="#" variant="body2">
-          Forgot Password?
-        </Link>
-        <Box mt={1}>
+        <Typography variant="h5" component="div" sx={{ mb: 2 }}>
+          Login Form
+        </Typography>
+        <TextField
+          fullWidth
+          label="Username"
+          {...register("username", {
+            required: "Username is required",
+            minLength: {
+              value: 3,
+              message: "Username must be at least 3 characters",
+            },
+          })}
+          error={Boolean(errors.username)}
+          helperText={errors.username?.message}
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          type="password"
+          label="Password"
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters",
+            },
+          })}
+          error={Boolean(errors.password)}
+          helperText={errors.password?.message}
+          margin="normal"
+          sx={{ mt: 2 }}
+        />
+        <FormControlLabel
+          control={<Checkbox {...register("rememberMe")} color="primary" />}
+          label="Remember Me"
+          sx={{ mt: 1, textAlign: "left" }}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{ mt: 2 }}
+        >
+          Login
+        </Button>
+        <Box sx={{ mt: 2, textAlign: "center" }}>
           <Link href="#" variant="body2">
-            Do not have an account? Sign Up
+            Forgot Password?
           </Link>
+          <Box mt={1}>
+            <Link href="#" variant="body2">
+              Do not have an account? Sign Up
+            </Link>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
