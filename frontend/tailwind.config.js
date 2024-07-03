783dd9212c4f3728be63c587c1bddba7b46b4769
Krmjn09 <kmahajan1_be22@thapar.edu>
2024-07-03 14:12:00 +0530

    commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // For React components
+    "./public/index.html", // For the main HTML file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
